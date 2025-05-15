e8bc3afaa189e721414f02c47b9e2e5a2974cbbc
MG1350 <114371390+MG1350@users.noreply.github.com>
2025-05-15 14:51:18 -0400

    Update Wordle.java

--- WordleStarter/Wordle.java
+++ WordleStarter/Wordle.java
@@ -13,6 +13,11 @@ public class Wordle {
     public void run() {
         gw = new WordleGWindow();
         gw.addEnterListener((s) -> enterAction(s));
+        randomWord = WordleDictionary.FIVE_LETTER_WORDS[(int) (Math.random()*WordleDictionary.FIVE_LETTER_WORDS.length)];
+        for(int i = 0; i <= WordleGWindow.N_COLS-1; i++)
+        {
+            gw.setSquareLetter(0, i, randomWord.substring(i, i+1));
+        }
     }
 
 /*
@@ -21,7 +26,39 @@ public class Wordle {
  */
 
     public void enterAction(String s) {
-        gw.showMessage("You have to implement this method.");
+        boolean real = false;
+        String[] letters = {"#", "#", "#", "#", "#"};
+        for (String word : WordleDictionary.FIVE_LETTER_WORDS)
+        {
+            if(s.equalsIgnoreCase(word))
+            {
+                real = true;
+            }
+        }
+        if(!real)
+        {
+            gw.showMessage("Not in word list");
+        }
+        else
+        {
+            gw.showMessage("Nice guess");
+            for(int i = 0; i < 5; i++)
+            {
+                boolean letterInList = false;
+                for(String letta : letters)
+                {
+                    if(letta.equalsIgnoreCase(s.substring(i,i+1)))
+                    {
+                        letterInList = true;
+                    }
+                }
+                if(!letterInList)
+                {
+                    letters[i] = s.substring(i,i+1);
+                }
+            }
+            System.out.println(letters[0] + letters[1] + letters[2] +letters[3] +letters[4]);
+        }
     }
 
 /* Startup code */
@@ -33,5 +70,5 @@ public class Wordle {
 /* Private instance variables */
 
     private WordleGWindow gw;
-
+    private String randomWord;
 }
